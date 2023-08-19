9bb11777170477064cdd065ec6742fdbc1178f05
DiyarKara <karadiyar66@gmail.com>
2023-08-19 23:37:05 +0300

    quest updates

--- Assets/Scripts/SecmenRejectAccept.cs
+++ Assets/Scripts/SecmenRejectAccept.cs
@@ -12,12 +12,40 @@ public class SecmenRejectAccept : MonoBehaviour
     public bool isPartyA_MissionAccepted = false;
     [HideInInspector]
     public int rejectedPartyA = 0;
+    public int rejectedPartyAFalse = 0;
+    public int rejectedPartyB = 0;
+    public int rejectedPartyBFalse = 0;
+
+    [HideInInspector]
+    public bool isPartyB_MissionEnded = false;
+    [HideInInspector]
+    public bool isPartyB_MissionAccepted = false;
+
+
     // Start is called before the first frame update
     public void SecmenReject()
     {
         isRejected += 1;
-        rejectedPartyA++;
-        Debug.Log("Rejected no: " + rejectedPartyA);
+        if(gameObject.tag == "A")
+        {
+            rejectedPartyAFalse++;
+            rejectedPartyA++;
+        }
+        if(gameObject.tag == "A_Fraud")
+        {
+            rejectedPartyA++;
+        }
+        if (gameObject.tag == "B")
+        {
+            rejectedPartyBFalse++;
+            rejectedPartyB++;
+        }
+        if (gameObject.tag == "B_Fraud")
+        {
+            rejectedPartyB++;
+        }
+
+        //Debug.Log("Rejected no: " + rejectedPartyA);
     }
 
     public void SecmenAccept()
@@ -29,15 +57,42 @@ public class SecmenRejectAccept : MonoBehaviour
         isPartyA_MissionAccepted = true;
         Debug.Log("Quest accepted");
     }
+
     public void PartAQuestEnded()
     {
         isPartyA_MissionEnded = true;
-        Debug.Log("Quest completed");
+        Debug.Log("Quest completed A: " + rejectedPartyBFalse);
+        Debug.Log("Quest completed A: " + rejectedPartyB);
+        Debug.Log("B: " + rejectedPartyAFalse);
+        Debug.Log("B: " + rejectedPartyA);
     }
+
     public void PartyAQuestFailed()
     {
         isPartyA_MissionEnded = false;
         Debug.Log("Quest failed");
     }
+
+    public void AcceptPartyBQuest()
+    {
+        isPartyB_MissionAccepted = true;
+        Debug.Log("Quest accepted");
+    }
+
+    public void PartBQuestEnded()
+    {
+        isPartyB_MissionEnded = true;
+        Debug.Log("Quest completed B: " + rejectedPartyAFalse);
+        Debug.Log("Quest completed B: " + rejectedPartyA);
+        Debug.Log("A: " + rejectedPartyBFalse);
+        Debug.Log("A: " + rejectedPartyB);
+    }
+
+    public void PartyBQuestFailed()
+    {
+        isPartyB_MissionEnded = false;
+        Debug.Log("Quest failed");
+    }
+
 
 }
